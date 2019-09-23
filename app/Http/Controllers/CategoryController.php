fa3fb57acdef8077427ecb3a8f9a4c051ac3eb9e
Sunkanmi Olukitibi <sunkanmikitibi@gmail.com>
2019-09-23 13:45:29 +0100

    new changes

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\Category as CategoryResource;
+use App\Http\Resources\CategoryCollection;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        //
+    }
 }
--- app/Http/Resources/CategoryCollection.php
+++ app/Http/Resources/CategoryCollection.php
@@ -14,6 +14,9 @@ class CategoryCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        //return parent::toArray($request);
+        return [
+            'data' => $this->collection
+        ];
     }
 }
